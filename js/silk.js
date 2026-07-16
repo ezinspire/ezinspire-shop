a5317a6334fcf1a804ff8a24dc62c4fb4f137be0
ezinspire <ezinspire.ltd@gmail.com>
2026-07-16 19:25:10 +0100

    Update silk.js to fix double description

--- js/silk.js
+++ js/silk.js
@@ -15,19 +15,25 @@ function drawSilkAnimation() {
     ctx.clearRect(0, 0, width, height);
     ctx.beginPath();
     ctx.moveTo(0, height * 0.5);
+    let lowestWavePoint = height * 0.5;
 
     for (let i = 0; i < width; i++) {
         const waveOne = Math.sin(i * 0.003 + step) * 90;
         const waveTwo = Math.cos(i * 0.002 + step * 0.8) * 40;
         const yPosition = (height * 0.52) + waveOne + waveTwo;
+        
         ctx.lineTo(i, yPosition);
+        
+        if (yPosition > lowestWavePoint) {
+            lowestWavePoint = yPosition;
+        }
     }
 
     ctx.lineTo(width, height);
     ctx.lineTo(0, height);
     ctx.closePath();
 
-    const linearGradient = ctx.createLinearGradient(0, height * 0.3, width, height);
+    const linearGradient = ctx.createLinearGradient(0, lowestWavePoint - 130, width, height);
     linearGradient.addColorStop(0, 'rgba(238, 235, 230, 0.4)');
     linearGradient.addColorStop(0.5, 'rgba(224, 219, 211, 0.2)');
     linearGradient.addColorStop(1, 'rgba(242, 240, 236, 0.7)');
@@ -38,7 +44,5 @@ function drawSilkAnimation() {
     step += 0.004; 
     requestAnimationFrame(drawSilkAnimation);
 }
-
-drawSilkAnimation();
 
 drawSilkAnimation();
